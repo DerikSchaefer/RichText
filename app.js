b07666f1b3f1343a99708e62f739203cc0bc6313
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2020-12-21 15:18:04 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "I created this blog during an online coding bootcamp I did on Udemy. I'm going to use it for the forseeable future to document my progress while learning how to code. One of the best ways to learn & retain is to teach so I think by writing 'articles' on some of the stuff I'm learning I will retain the information better + I'll be able to go back and reference them. It also might be fun to look back on this once I'm a full fledged SE & see where I was at this point.";
+const aboutContent = "My name is Derik Schaefer & I'm currently Active Duty in the Coast Guard but pursuing a career change into software engineering in the greater Seattle area.";
+const contactContent = "";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://Derik-Admin:Test123@cluster0.brkbu.mongodb.net/Blog", {useNewUrlParser: true});
+
+
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started");
+});
